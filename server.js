a1cd51acca2413efe925f8e592324f3cad410e6f
ANDRES KWAN <andreskwan@gmail.com>
2014-09-25 17:44:53 -0500

    Paso1 - finished

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
+/**
+* Dependencies
+*/
 var express = require('express');
+
+/**
+* Local Variables
+*/
+
 //se exporta el servicio - server para que las pruebas lo consuman
 var server = module.exports = express();
+var port   = process.env.PORT || 3000;
+/**
+* Routes
+*/
 
+//con esto reponderemos a un post hecho a /notas
 server.post('/notas', function (req, res){
+	//1 recibir lo que nos envian en el POST
+	//- para poder analizar esto necesitamos 
+	//bodyparser 
 	res
 	.status(201)
-	.json({}); 
+	.send({
+			"nota": {
+					'title': "Mejorando.la #node-pro",
+					"description": "Introduccion a clase",
+					"type": "js", // tipo de dato de la nota, permitir highlight and warnings 
+					"body": "soy el cuerpo de json",
+					"id":"pepe"
+				}}); 
 });
 
 //to work with supertest
@@ -13,8 +36,8 @@ server.post('/notas', function (req, res){
 //o somos el servidor 
 //si tengo padre, me estan usando como modulo
 if (!module.parent) {
-	server.listen(3000, function (){
-		console.log('http://localhost:3000');
+	server.listen(port, function (){
+		console.log('http://localhost:'+port);
 	});
 }else{
 	module.exports = server;
--- test/notas.js
+++ test/notas.js
@@ -14,40 +14,48 @@ request = request(host);
 describe('recurso /notas', function (){
 	//La primera prueba sera POST
 	describe('POST', function (){
-		it('should create a new note', function (done){
+		it('should return/create a new note', function (done){
 			// throw new Error('tengo hambre'); 
 			// return true;
 			//crear nota nueva
 			var data = {
-				//"nota": {
+				"nota": {
 					"title": "Mejorando.la #node-pro",
 					"description": "Introduccion a clase",
 					"type": "js", // tipo de dato de la nota, permitir highlight and warnings 
 					"body": "soy el cuerpo de json"
 				}
+			};
 			//};
 			//usar supertest para hacer request
-			//1 crear solicitud de http enviando data
+			//1 crear solicitud de http/POST enviando data
 			request
+				//------------------
+				//Post - send - create
+				//------------------
 				.post('/notas')
 				//format: usar el encabezado para identificar el recurso 
 				//         accept application/json
 				.set('Accept', 'application/json')
 				//body: nota en json
 				.send(data)
+				//------------------
+				//Resp - from node.js server
+				//------------------
 				//status code
 				//pasar nuestras expectativas
 				.expect(201)
 				.expect('Content-Type', /application\/json/)
+				//callback para evaluar el body
 				.end(function (err, res){
 					var body = res.body;
+					console.log('body: \n'+ body);
 
+					//does the note exist?
 					expect(body).to.have.property('nota');
-
-					nota = body.notas;
-
+					nota = body.nota;
 					expect(nota).to.have.property('title', 'Mejorando.la #node-pro');
-					expect(nota).to.have.property('description', 'Introcuccion a clase');
+					expect(nota).to.have.property('description', 'Introduccion a clase');
 					expect(nota).to.have.property('type', 'js');
 					expect(nota).to.have.property('body', 'soy el cuerpo de json');
 					expect(nota).to.have.property('id');
@@ -58,6 +66,7 @@ describe('recurso /notas', function (){
 				// 	var body = res.body;
 				// 	expect(body).to.have.property('nota');
 		});
+		//no pasa, no funciona la asynchronia
 		it.skip('should evaluate async', function (done){
 			  //1 codigo 
 			  var mejorandola = 'prueba';
