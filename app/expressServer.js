29f08962f0fef9334a9a103784d05034355fea6e
ANDRES KWAN <andreskwan@gmail.com>
2014-11-26 19:09:23 -0500

    adv - v1.5 - template - consolidate - swig - ROUTES - RESTnotas & get

--- app/expressServer.js
+++ app/expressServer.js
@@ -1,5 +1,7 @@
-var express = require('express'); 
+var express     = require('express'); 
 var middlewares = require('./middlewares/admin.js');
+var cons		= require('consolidate');
+var RESTnotas   = require('../lib/notas');
 //creando el objeto
 var ExpressServer = function (config){
 	//si vacio, es un objeto vacio
@@ -10,6 +12,26 @@ var ExpressServer = function (config){
 	for (var middleware in middlewares){
 		this.server.use(middlewares[middleware]);
 	}
+	/**
+	*	template engine - Swig
+	*/
+	//
+	//define engine
+	this.server.engine('html', cons.swig);
+	//como template de vista use html y asi asocia a swig
+	this.server.set('view engine', 'html');
+	//donde estan los html?
+	this.server.set('views', __dirname + '/website/views/templates');
+	/**
+	*	routes
+	*/
+	//Model REST 
+	this.server.use(RESTnotas);
+	//view
+	this.server.get('/', function (req, res, next){
+		// debugger;
+		res.render('article_save', {nombre:"Andres"});
+	});
 }; 
 
 module.exports = ExpressServer;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 /**
 * Dependencies
 */
-
 var logger        = require('./lib/logger/logger.js');
 var ExpressServer = require('./app/expressServer.js')
 var conf          = require('./conf.json');
@@ -9,18 +8,13 @@ var conf          = require('./conf.json');
 * Local Variables
 */
 var app = new ExpressServer();
-//se exporta el servicio - server para que las pruebas lo consuman
-// var server = module.exports = express();
 var port   = process.env.PORT || conf.port;
-/**	
-* Middleware
-*/
-// app.server.use(bodyParser.json('application/json'));
 /**
 * Routes
 */
-var notas = require('./lib/notas');
-app.server.use(notas);
+// var notas = require('./lib/notas');
+// app.server.use(notas);
+
 //to work with supertest
 //supertest nos esta usando como modulo?
 //o somos el servidor 
@@ -30,5 +24,6 @@ if (!module.parent) {
 		// logger.info('http	://localhost:'+port);
 	});
 }else{
+	//se exporta el servicio - server para que las pruebas lo consuman
 	module.exports = app.server;
 }
