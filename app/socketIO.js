53761a9af0f817b70916724442b320d5660dbc1b
ANDRES KWAN <andreskwan@gmail.com>
2014-12-08 16:13:07 -0500

    ad - v3.1.2 - socket.io and express working together - changed server.js

--- app/socketIO.js
+++ app/socketIO.js
@@ -2,7 +2,8 @@ var IO = require('socket.io');
 
 var SocketIO = function (conf) {
 	conf = conf || {};
-	
+	var io = IO.listen(conf.server);
+
 }
 
 module.exports = SocketIO;
--- server.js
+++ server.js
@@ -2,15 +2,19 @@
 * Dependencies
 */
 var logger        = require('./lib/logger/logger.js');
+var http          = require('http');
 var ExpressServer = require('./app/expressServer.js')
 var mongoose      = require('mongoose');
+var socketIO      = require('./app/socketIO.js');
 var conf          = require('./conf.json'); 
 
 /**
 * Local Variables
 */
-var express  = new ExpressServer();
-var port = process.env.PORT || conf.port;
+var port    = process.env.PORT || conf.port;
+var express = new ExpressServer();
+var server  = http.createServer(express.app);
+var Io      = new socketIO({server:server});
 
 mongoose.connect('mongodb://'+conf.mongoDB.host+'/'+conf.mongoDB.name);
 //to work with supertest
@@ -18,10 +22,10 @@ mongoose.connect('mongodb://'+conf.mongoDB.host+'/'+conf.mongoDB.name);
 //o somos el servidor 
 //si tengo padre, me estan usando como modulo
 if (!module.parent) {
-	express.app.listen(port, function (){
+	server.listen(port, function (){
 		logger.info('http://localhost:'+port);
 	});
 }else{
 	//se exporta el servicio - app para que las pruebas lo consuman
-	module.exports = express.app;
+	module.exports = server;
 }
