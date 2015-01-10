821e71de466af347c0dd9c54301293e35e84cdad
ANDRES KWAN <andreskwan@gmail.com>
2015-01-10 18:49:22 -0500

    Header + menu - fixed -- scripts fixed -- img sizes defined

--- app/static/js/mainSocket.js
+++ app/static/js/mainSocket.js
@@ -12,12 +12,3 @@ $(document).ready(function (){
 		io.emit('mejorandolo', {hola:"Emit desde el browser"});
 	});
 });
-
-document.getElementById('links').onclick = function (event) {
-    event = event || window.event;
-    var target = event.target || event.srcElement,
-        link = target.src ? target.parentNode : target,
-        options = {index: link, event: event},
-        links = this.getElementsByTagName('a');
-    blueimp.Gallery(links, options);
-};
